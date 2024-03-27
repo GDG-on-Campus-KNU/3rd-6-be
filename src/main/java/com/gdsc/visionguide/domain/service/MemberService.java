868be1a8dbbf5fcf0277c657ef7e2cc88d849d25
이민혁 <helloaway213@gmail.com>
2024-03-27 20:50:22 +0900

    자체 로그인 구현

--- src/main/java/com/gdsc/visionguide/domain/service/MemberService.java
+++ src/main/java/com/gdsc/visionguide/domain/service/MemberService.java
@@ -1,8 +1,10 @@
 package com.gdsc.visionguide.domain.service;
 
+import com.gdsc.visionguide.auth.itself.dto.request.SignUpRequestDto;
 import com.gdsc.visionguide.domain.entity.Member;
 import com.gdsc.visionguide.domain.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,6 +16,7 @@ import java.util.Optional;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public Member saveOrUpdate(Member member) {
         Optional<Member> optionalExistingMember = memberRepository.findByEmail(member.getEmail());
@@ -25,6 +28,24 @@ public class MemberService {
             return memberRepository.save(existingMember);
         } else {
             return memberRepository.save(member);
+        }
+    }
+
+    public void signUp(SignUpRequestDto signUpRequest) {
+
+        Optional<Member> optionalExistingMember = memberRepository.findByEmail(signUpRequest.email());
+
+        // 이메일이 중복되지 않으면
+        if (!optionalExistingMember.isPresent()) {
+            Member member = Member.builder()
+                    .name(signUpRequest.name())
+                    .email(signUpRequest.email())
+                    .password(passwordEncoder.encode(signUpRequest.password()))
+                    .build();
+
+            memberRepository.save(member).getId();
+        } else {
+            throw new RuntimeException("member email이 이미 존재합니다");
         }
     }
 }
