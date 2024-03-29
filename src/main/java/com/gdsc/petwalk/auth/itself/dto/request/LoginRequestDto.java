71c8374180b0357cb8bab634de538595baa754c4
이민혁 <helloaway213@gmail.com>
2024-03-29 20:16:18 +0900

    Rename: 프로젝트 명 수정
    visionguide -> petwalk

---
+++ src/main/java/com/gdsc/petwalk/auth/itself/dto/request/LoginRequestDto.java
@@ -0,0 +1,4 @@
+package com.gdsc.petwalk.auth.itself.dto.request;
+
+public record LoginRequestDto(String email, String password) {
+}
--- src/main/java/com/gdsc/visionguide/auth/itself/dto/request/LoginRequestDto.java
+++
@@ -1,4 +0,0 @@
-package com.gdsc.visionguide.auth.itself.dto.request;
-
-public record LoginRequestDto(String email, String password) {
-}
