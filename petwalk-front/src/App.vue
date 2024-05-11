55f1b7bf0ff99c4af8010f801fce7926322f2d63
이민혁 <helloaway213@gmail.com>
2024-05-11 16:37:54 +0900

    Feat : 예외 처리 basic -> 순차적으로 추가

---
+++ petwalk-front/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <a href="http://localhost:8080/oauth2/authorization/google">구글 로그인하기</a>
+  <p>accesstoken : {{accessToken}}</p>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      accessToken: null,
+    };
+  },
+  mounted() {
+    this.accessToken = this.$cookies.get('Authorization-refresh');
+    console.log(this.accessToken);
+  },
+};
+</script>
+
+<style>
+
+</style>
