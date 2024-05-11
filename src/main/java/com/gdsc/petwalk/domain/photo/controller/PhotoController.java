85dfa1a3a9690d8ff10dd32aedfacc8d8a97d2c4
이민혁 <helloaway213@gmail.com>
2024-05-11 20:04:40 +0900

    Feat : Photo 관련 도메인

---
+++ src/main/java/com/gdsc/petwalk/domain/photo/controller/PhotoController.java
@@ -0,0 +1,14 @@
+package com.gdsc.petwalk.domain.photo.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/photo")
+@RequiredArgsConstructor
+public class PhotoController {
+
+
+
+}
