8623cace3896b413093c457efa5e120b2018022a
이민혁 <helloaway213@gmail.com>
2024-05-15 20:13:29 +0900

    Feat : JwtAuthorizationFilter 수정, JwtExceptionHandlerFilter 구현

---
+++ src/main/java/com/gdsc/petwalk/auth/jwt/filter/JwtExceptionHandlerFilter.java
@@ -0,0 +1,43 @@
+package com.gdsc.petwalk.auth.jwt.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gdsc.petwalk.global.exception.NotValidTokenException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+public class JwtExceptionHandlerFilter extends OncePerRequestFilter {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            filterChain.doFilter(request, response);
+        } catch (NotValidTokenException e){
+            HttpStatus httpStatus = e.getErrorCode().getHttpStatus();
+            String message = e.getErrorCode().getMessage();
+
+            Map<String, Object> jsonResponse = new HashMap<>();
+            jsonResponse.put("status", httpStatus.value());
+            jsonResponse.put("message", message);
+
+            response.setStatus(httpStatus.value());
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+
+            objectMapper.writeValue(response.getWriter(), jsonResponse);
+        }
+    }
+}
