2e0a657ce613e39022bf3cad09ee79e892c45d43
이민혁 <helloaway213@gmail.com>
2024-05-15 20:09:13 +0900

    Feat : Controller 예외 처리 로직 - 추후 구체화

---
+++ src/main/java/com/gdsc/petwalk/global/exception/ExControllerAdvice.java
@@ -0,0 +1,11 @@
+package com.gdsc.petwalk.global.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class ExControllerAdvice {
+    // 컨트롤러 단에서 발생하는 예외 처리
+
+}
