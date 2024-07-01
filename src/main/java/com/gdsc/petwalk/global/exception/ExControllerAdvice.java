55e7a96d8bce31e75344f98e2930e6bfa53c5308
이민혁 <helloaway213@gmail.com>
2024-07-01 19:13:56 +0900

    Chore : Home 화면 관련 예외처리 추가

--- src/main/java/com/gdsc/petwalk/global/exception/ExControllerAdvice.java
+++ src/main/java/com/gdsc/petwalk/global/exception/ExControllerAdvice.java
@@ -10,18 +10,47 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.support.MissingServletRequestPartException;
 
+import java.time.format.DateTimeParseException;
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
 @Slf4j
 public class ExControllerAdvice {
 
+    // 클라이언트의 요청이 잘못 됨. dto 이름이나 양식 등
+    @ExceptionHandler(MissingServletRequestPartException.class)
+    public ResponseEntity<ExceptionDto> handleMissingServletRequestPartException(MissingServletRequestPartException e){
+        log.info("Request 요청 관련 오류");
+        ExceptionDto exceptionDto = ExceptionDto.builder()
+                .status(false)
+                .message(e.getMessage())
+                .errorCode(HttpStatus.BAD_REQUEST.toString())
+                .build();
+
+        return ResponseEntity.badRequest().body(exceptionDto);
+    }
+
+    @ExceptionHandler(DateTimeParseException.class)
+    public ResponseEntity<ExceptionDto> DateTimeParseException(DateTimeParseException e){
+        log.info("LocalDateTime의 형식이 잘 못 되었습니다");
+        ExceptionDto exceptionDto = ExceptionDto.builder()
+                .status(false)
+                .message(e.getMessage())
+                .errorCode(HttpStatus.BAD_REQUEST.toString())
+                .build();
+
+        return ResponseEntity.badRequest().body(exceptionDto);
+    }
+
     // 컨트롤러 단에서 발생하는 예외 처리
     @ExceptionHandler(MemberException.class)
     public ResponseEntity<ExceptionDto> DuplicateEmailException(MemberException e){
         log.info("Email 중복 오류 예외 핸들러 발생");
         ExceptionDto exceptionDto = ExceptionDto.builder()
+                .status(false)
                 .message(e.getErrorCode().getMessage())
                 .errorCode(e.getErrorCode().getHttpStatus().toString())
                 .build();
@@ -33,6 +62,7 @@ public class ExControllerAdvice {
     public ResponseEntity<ExceptionDto> NotValidEmailException(MethodArgumentNotValidException e){
         log.info("회원가입 시 형식 오류 예외 핸들러 발생");
         ExceptionDto exceptionDto = ExceptionDto.builder()
+                .status(false)
                 .message(e.getBindingResult().getFieldErrors().get(0).getDefaultMessage())
                 .errorCode(e.getStatusCode().toString())
                 .build();
@@ -44,11 +74,24 @@ public class ExControllerAdvice {
     public ResponseEntity<ExceptionDto> UsernameNotFoundException(UsernameNotFoundException e){
         log.info("DB에서 유저 조회 시 예외 핸들러 발생");
         ExceptionDto exceptionDto = ExceptionDto.builder()
+                .status(false)
                 .message(e.getMessage())
                 .errorCode(HttpStatus.BAD_REQUEST.toString())
                 .build();
 
         return ResponseEntity.badRequest().body(exceptionDto);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<ExceptionDto> NoSuchElementException(NoSuchElementException e){
+        log.info("DB에서 엔티티 조회 시 예외 핸들러 발생");
+        ExceptionDto exceptionDto = ExceptionDto.builder()
+                .status(false)
+                .message(e.getMessage())
+                .errorCode(HttpStatus.NOT_FOUND.toString())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exceptionDto);
     }
 
 }
